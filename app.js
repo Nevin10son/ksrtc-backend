a096534d68ade652d9415b9bee6b88b4d3233f74
Nevin10son <nevintenson235@gmail.com>
2024-06-14 08:14:56 +0530

    login api development

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require("express")
+const mongoose1 = require("mongoose")
 const mongoose = require("mongoose")
 const cors = require("cors")
 const {busmodel} = require("./model/bus")
+const {usermodel} = require("./model/user")
+const jwt =require("jsonwebtoken")
+
 const bcrypt = require("bcryptjs")
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect('mongodb+srv://Nevin:nevintensonk@cluster0.0rfrr.mongodb.net/cinemadb?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect('mongodb+srv://Nevin:nevintensonk@cluster0.0rfrr.mongodb.net/userkdb?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect('mongodb+srv://Nevin:nevintensonk@cluster0.0rfrr.mongodb.net/busdb?retryWrites=true&w=majority&appName=Cluster0')
 
 
 const generateHashedPassword = async(password)=>{
@@ -19,11 +24,16 @@ app.post("/signup",async(req,res)=>{
     let input = req.body
     let hashedPassword = await generateHashedPassword(input.password)
     console.log(hashedPassword)
-    let bus = new busmodel(input)
-    bus.save()
+    let user = new usermodel(input)
+    user.save()
     res.json({"status":"success"})
     
 })
+
+
+    
+
+
 
 app.listen(8088,()=>{
     console.log("server started")
--- model/bus.js
+++ model/bus.js
@@ -1,15 +1,14 @@
 const mongoose = require("mongoose")
-const schema = mongoose.Schema({
-    "name":{type:String,required:true},
-    "email":{type:String,required:true},
-    "phoneno":{type:String,required:true},
-    "gender":{type:String,required:true},
-    "password":{type:String,required:true},
+
+
+const schema1 = mongoose.Schema({
     "busname":{type:String,required:true},
     "route":{type:String,required:true},
     "busno":{type:Number,required:true},
     "drivername":{type:String,required:true}
 })
 
+
 let busmodel = mongoose.model("buses",schema)
 module.exports = {busmodel}
+
